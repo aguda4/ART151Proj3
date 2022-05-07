f5de6c57a8f37bff16a5b05449ff02c9dc2c4b86
aguda4 <aguda4@uic.edu>
2022-05-07 18:01:39 -0500

    ok

--- anime.js
+++ anime.js
@@ -1,4 +1,5 @@
 //console.log(navigator)
+let audio = new Audio('sounds/testing.mp3');
 
 if(navigator.requestMIDIAccess) {
     navigator.requestMIDIACCess().then(success, failure);
@@ -56,11 +57,19 @@ function noteOn(note) {
         b = 10;
         document.getElementById('testelm').style.backgroundColor = `rgb(0,0,${b},1)`;
     }
-    if (note == 99){
+    if (note == 90){
         b = 225;
         let p5_ = new p5();
         document.getElementById('testelm').style.backgroundColor = `rgb(0,0,${b},1)`;
 
+    }
+    if (note == 99)
+    {
+        b = 225;
+        audiopause();
+        let p5_ = new p5();
+        console.log(p5_.map(0.5,0,1,0,100));
+        document.getElementById('testelm').style.backgroundColor = `rgb(0,0,${b},1)`;
     }
     }
 
@@ -70,4 +79,61 @@ function noteOff(note) {
     if(note == 64){
         document.getElementById("testelm").innerHTML = "Back to normal"
     }
+    if(note == 84) {
+        document.getElementById('testelm').style.backgroundColor = `rgb(255,255,255,1)`;
+    }
+    if(note == 94) {
+        audiopause();
+    }
+}
+function audioplay(){
+    audio.play();
+}
+function audiopause(){
+    audio.pause();
+}
+
+function apicallValue() {
+    //lat = '41.878';
+    //lon = '-87.629';
+    //APIkey = 'abe4d2e4b7080694f170017c8e38a045'
+    //units = 'imperial';
+    //https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&uints=${units}`, //hardcode everything
+    apikey = '8c7dc264ab34e705e3e68e53ff44a3c02bef1b4189756db57ae750d949fabdae';
+    search = 'cat';
+
+ $.ajax({
+     type: 'GET',
+     //GET POST
+     datatype: 'json',
+     //url: 'http://api.open-notify.org/iss-now.json',
+     //url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&uints=${units}`,
+     url: `https://serpapi.com/search.json?q=${search}&tbm=isch&ijn=0&api_key=${apikey}`,
+     async: false,
+     crossDomain: true,
+     complete: function (data) {
+
+         if (response.readyState == 4 && response.status == 200) {
+             console.log(response);
+            // Lat == response.responseJSON.iss_position.latitude;
+             //Long == response.responseJSON.iss_position.longitude;
+           //  temp = response.responseJSON.main.temp;
+            // desc = response.responseJSON.weather[0].description; //weather is in the array
+            // console.log(desc);
+             //console.log(temp);
+             //console.log(Lat);
+             //console.log(Long);
+
+
+         /*if (data.readyState == 4 && data.status == 200) {
+             console.log(data);
+             Lat == data.responseJSON.iss_position.latitude;
+             Long == data.responseJSON.iss_position.longitude;*/
+             //
+             //console.log(remap(Lat));
+             //console.log(remap(Long));
+     
+         }
+     }
+ });
 }
