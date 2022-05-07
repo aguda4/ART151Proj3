0ce46305e66fc7d7d83733db48be7c8517b0ab90
aguda4 <aguda4@uic.edu>
2022-05-06 20:28:58 -0500

    ok

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-//console.log(navigator)
-
 if(navigator.requestMIDIAccess) {
     navigator.requestMIDIACCess().then(success, failure);
-}
-
-function failure(){
-    console.log('Could not connect MIDI');
-}
-
-function updateDevices(event)
-{
-    console.log(event);
 }
 
 function success(midiAccess){
     //console.log(midiAccess);
     midiAccess.addEventListener('statechange', updatedDevices);
     const inputs = midiAccess.inputs; 
-    console.log(inputs);
+    //console.log(inputs);
 
     inputs.forEach((input) => {
         //console.log(input);
         input.addEventListener('midimessage', handleInput);
     })
 
+    
+}
+function failure(){
+    console.log('Could not connect MIDI');
+}
+
+function updateDevices(event)
+{
+    //console.log(event);
+    console.log(`Name: ${event.port.name}, Brand: ${event.port.manufacture}, State: ${event.port.state}, Type: ${event.port.type} `);
 }
 function handleInput(input) {
     //console.log(input);
@@ -39,35 +38,20 @@ function handleInput(input) {
     switch(command) {
         case 144:
             if(velocity > 0){
-                noteOn(note);
+                noteOn(note , velocity);
             } else {
                 noteOff(note);
             }
             break;
+            case 128:
+                noteOff(note);
+                break;
     }
 }
-function noteOn(note) {
 
-    console.log(`note:${note} //on`);
-    if (note == 64) {
-    document.getElementById("testelm").innerHTML = "Note 64 is On"
-    }
-    if (note == 96) {
-        b = 10;
-        document.getElementById('testelm').style.backgroundColor = `rgb(0,0,${b},1)`;
-    }
-    if (note == 99){
-        b = 225;
-        let p5_ = new p5();
-        document.getElementById('testelm').style.backgroundColor = `rgb(0,0,${b},1)`;
-
-    }
-    }
-
-
-function noteOff(note) {
-    console.log(`note:${note} //off`);
-    if(note == 64){
-        document.getElementById("testelm").innerHTML = "Back to normal"
-    }
+function noteOn(note, velocity){
+    console.log(note, velocity);
+}
+function noteOff(note){
+    console.log(note);
 }
